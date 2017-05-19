da24dd58902968ccbdf446e058e22806d6b64a4d
lurind34 <lurind34@gmail.com>
2017-05-19 10:49:20 -0400

    Update test.rb
    made it do something

--- test.rb
+++ test.rb
@@ -1,3 +1,10 @@
 print "What is your age"
 age = gets.chomp
 age.capitalize!
+if age == "old"
+  print "damn you old af"
+elsif age == "young"
+  print "damn you too young to have a computer"
+else
+  print "oh"
+end
